64dabf4833428fde6373891eca35488e53a213f0
Valto Silva <valtosilva7@gmail.com>
2023-03-04 17:47:54 -0300

    CLIENT => Frontend Installation and Setup, React Redux File Folder Architecture and React Router, Redux and Toolkit Installation and Setup, Color, Theme, Dark Mode, and Styling Setup, Created Navbar Component

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import Login from "pages/auth/Login";
+import Profile from "pages/Profile";
+import Home from "pages/Home";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

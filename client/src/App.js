06a33099c7b28358f112b5e75e89b2c94843ef5e
Valto Silva <valtosilva7@gmail.com>
2023-03-06 13:01:09 -0300

    CLIENT => Register, Login Pages, and Form Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import Login from "pages/auth/Login";
 import Profile from "pages/Profile";
 import Home from "pages/Home";
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "theme";
+
+export const apiUrl = process.env.REACT_APP_BASE_URL;
 
 function App() {
   const mode = useSelector((state) => state.mode);
--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,5 +1,38 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "components/form/Form";
+
 const Login = () => {
-  return <div>Login</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box>
+      <Box width="100%" backgroundColor={theme.palette.background.alt}>
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+          p="1rem 6%"
+          textAlign="center"
+        >
+          Socialize
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socialize, the Social Media for Friendly People!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
